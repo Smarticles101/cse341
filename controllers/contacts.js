6ec50f9e82f5c2436c7bb90d01ae8bba7533afa5
Logan Stucki <loganstucki@gmail.com>
2023-07-19 15:51:50 -0600

    lesson 03

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -25,7 +25,61 @@ const getContactById = (req, res) => {
     });
 };
 
+const createContact = (req, res) => {
+  console.log(req);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  db.getDB()
+    .db()
+    .collection("contacts")
+    .insertOne(contact)
+    .then((result) => {
+      res.status(201).send(result);
+    });
+};
+
+const updateContact = (req, res) => {
+  const id = new ObjectId(req.params.id);
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  db.getDB()
+    .db()
+    .collection("contacts")
+    .replaceOne({ _id: id }, contact)
+    .then((result) => {
+      res.status(200).send(result);
+    });
+};
+
+const deleteContact = (req, res) => {
+  const id = new ObjectId(req.params.id);
+
+  db.getDB()
+    .db()
+    .collection("contacts")
+    .remove({ _id: id })
+    .then((result) => {
+      res.status(200).send(result);
+    });
+};
+
 module.exports = {
   getContacts,
   getContactById,
+  createContact,
+  updateContact,
+  deleteContact,
 };
