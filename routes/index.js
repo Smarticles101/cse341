9ae5999978f80ed238481603c9852fac3d44b178
Logan Stucki <loganstucki@gmail.com>
2023-07-19 16:03:58 -0600

    lesson 04

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const routes = require("express").Router();
-const controllers = require('../controllers/');
+const controllers = require("../controllers/");
 
-routes.use('/contacts', require('./contacts'));
+routes.use("/contacts", require("./contacts"));
+routes.use("/", require("./swagger"));
 
-routes.get('/', controllers.displayData);
+routes.get("/", controllers.displayData);
 
 module.exports = routes;
