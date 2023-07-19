5ecda745da1825adc964f70725daea64e7a189d4
Logan Stucki <loganstucki@gmail.com>
2023-07-19 15:21:38 -0600

    lesson 02

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// express server
+const db = require("./db/database");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// initialize the database
+db.init((err, db) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("Database initialized!");
+});
 
 // import the routes
 const routes = require("./routes");
